f0e03170b0c00aec325eaadcdbe3ee6c753fb020
Bryce Grebil <grebil@gmail.com>
2012-11-29 14:00:01 -0800

    Initial checkin.

---
+++ js/config.js
@@ -0,0 +1,14 @@
+var CONFIG = {
+	AVAILABLE_DBS: ["mssql", "mysql", "oracle", "postgresql", "sqlalchemy", "sqlite"],
+	DEFAULT_DB: "mssql",
+
+	AVAILABLE_LOCALES: ["en"],
+	DEFAULT_LOCALE: "en",
+	
+	RELATION_THICKNESS: 2,
+	RELATION_SPACING: 15,
+	RELATION_COLORS: ["#000", "#800", "#080", "#008", "#088", "#808", "#088"],
+	
+	STATIC_PATH: "",
+	XHR_PATH: ""
+}
